9ccf0925b89dac0e7f1f20dd1888fd9a38c2c341
c-cortex <82171816+c-cortex@users.noreply.github.com>
2021-05-03 00:59:28 -0700

    Update flask_vocab.py

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -5,7 +5,9 @@ from a scrambled string)
 """
 
 import flask
+from flask import request
 import logging
+
 
 # Our modules
 from src.letterbag import LetterBag
@@ -52,16 +54,18 @@ def index():
     app.logger.debug("At least one seems to be set correctly")
     return flask.render_template('vocab.html')
 
-
-@app.route("/keep_going")
-def keep_going():
-    """
-    After initial use of index, we keep the same scrambled
-    word and try to get more matches
-    """
-    flask.g.vocab = WORDS.as_list()
-    return flask.render_template('vocab.html')
-
+#######################
+# Keep_going was no longer needed
+#
+# @app.route("/keep_going")
+# def keep_going():
+#     """
+#     After initial use of index, we keep the same scrambled
+#     word and try to get more matches
+#     """
+#     flask.g.vocab = WORDS.as_list()
+#     return flask.render_template('vocab.html')
+#######################
 
 @app.route("/success")
 def success():
@@ -74,7 +78,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check")
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -87,19 +91,23 @@ def check():
     app.logger.debug("Entering check")
 
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    text = request.args.get("text", type=str)
     jumble = flask.session["jumble"]
     matches = flask.session.get("matches", [])  # Default to empty list
+    found = (text + " ") in matches # was word found
 
     # Is it good?
     in_jumble = LetterBag(jumble).contains(text)
     matched = WORDS.has(text)
 
     # Respond appropriately
-    if matched and in_jumble and not (text in matches):
+    if matched and in_jumble and not ((text + " ") in matches):
         # Cool, they found a new word
-        matches.append(text)
+        matches.append(text + " ")
         flask.session["matches"] = matches
+
+    # Moved into vocab.html
+    """
     elif text in matches:
         flask.flash("You already found {}".format(text))
     elif not matched:
@@ -110,12 +118,19 @@ def check():
     else:
         app.logger.debug("This case shouldn't happen!")
         assert False  # Raises AssertionError
+    """
 
     # Choose page:  Solved enough, or keep going?
+    # No longer needed with keyup, keep_going is not needed and success is done in vocab.html
+    """
     if len(matches) >= flask.session["target_count"]:
        return flask.redirect(flask.url_for("success"))
     else:
        return flask.redirect(flask.url_for("keep_going"))
+    """
+
+    rslt = {"in_jumble": in_jumble, "matched": matched, "found": found, "results": matches, "finished": len(matches) >= flask.session["target_count"]}
+    return flask.jsonify(result=rslt)
 
 
 ###############
